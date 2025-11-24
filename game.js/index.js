acd7f8cca1fb5710f871a7ba18523a8816c7345b
flowberrys <156359194+flowberrys@users.noreply.github.com>
2025-11-24 12:21:16 -0500

    g

--- game.js/index.js
+++ game.js/index.js
@@ -7,7 +7,7 @@ canvas.height = 576;
 c.fillRect(0,0,canvas.width,canvas.height)
 
 class Sprite {
-    constructor ({position,velocity,health,attack,movalbilty}) {
+    constructor ({position,velocity,health,attack,movalbilty_for_placement_towers}) {
         this.position = position
         this.velocity = velocity
         this.width = 50;
@@ -16,7 +16,8 @@ class Sprite {
         this.draw = c.fillRect(this.position.x,this.position.y,this.width,this.height);
         this.health = this.health;
         this.attack = this.attack;
-        this.movalbilty_for_placement = false
+        this.movalbilty = false;
+        movalbilty_for_placement_towers = false;
         
         
 
@@ -74,6 +75,22 @@ const hard_enemy = new Sprite({
 
     color: 'red'
     
+})
+
+const basic_tower = new Sprite({
+position: {
+    x:0,
+    y:0
+},
+velocity:{
+    x:0,
+    y:0,
+},
+health: 200,
+
+attack: 5,
+
+
 })
 
 
