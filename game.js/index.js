57d359a14eaa5b1630ad63438d86bb91993d88f2
flowberrys <156359194+flowberrys@users.noreply.github.com>
2025-11-25 12:28:40 -0500

    added better tower

--- game.js/index.js
+++ game.js/index.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d')
+const c = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
 canvas.width = 1024;
 canvas.height = 576;
@@ -14,7 +15,7 @@ class Sprite {
         this.height = 50;
         this.color = c.fillStyle(this.color);
         this.attack_radius = this.attack_radius
-        this.draw = (c.fillRect(this.position.x,this.position.y,this.width,this.height), c.fillCircle(this.position.x,this.position.y,this.attack_radius) );
+        this.draw = (c.fillRect(this.position.x,this.position.y,this.width,this.height),ctx.fillStyle = "#ADD8E6", ctx.fillStyle.opacity = 0.5 ,  ctx.fillCircle(this.position.x,this.position.y,this.attack_radius) );
         this.health = this.health;
         this.attack = this.attack;
         this.movalbilty = false;
@@ -79,9 +80,10 @@ const hard_enemy = new Sprite({
     color: 'red'
     
 })
+//buttons for towers
 const basic_btn = document.getElementById(basic_btn)
-
-
+const better_btn = document.getElementById(better_btn)
+//const for basic tower
 const basic_tower = new Sprite({
 position: {
     x:0,
@@ -91,7 +93,7 @@ velocity:{
     x:0,
     y:0,
 },
-health: 200,
+health: Infinity,
 
 attack: 5,
 
@@ -100,11 +102,29 @@ movalbilty_for_placement_towers: false,
 attack_radius: 10,
 
 })
+//const for better tower
+const better_tower = new Sprite({
+position: {
+    x:0,
+    y:0
+},
+velocity:{
+    x:0,
+    y:0,
+},
+health: Infinity,
 
+attack: 15,
+
+movalbilty_for_placement_towers: false,
+
+attack_radius: 50,
+
+})
 addEventListener(MouseEvent('click'))
 addEventListener(mouseX)
-
-if(MouseEvent(mouseX) <= 70 && MouseEvent(mouseY) >= 506 && MouseEvent(click) === true){
+//basic tower control and placement
+if(MouseEvent(mouseX) <= 69 && MouseEvent(mouseY) >= 506 && MouseEvent(click) === true){
     basic_tower.movalbilty_for_placement_towers = true
     basic_tower.position.x = MouseEvent(mouseX)
     basic_tower.position.y = MouseEvent(mouseY)
@@ -116,20 +136,45 @@ if(MouseEvent(mouseX) <= 70 && MouseEvent(mouseY) >= 506 && MouseEvent(click) ==
         basic_tower.position.x = MouseEvent(mouseX)
     }
 
-    if(MouseEvent('click') === true){
+    if(MouseEvent('click') === true && coins >= 19){
+
+        basic_tower.movalbilty_for_placement_towers === false
         basic_tower.draw(
         basic_tower.position.x = mouseX,
         basic_tower.position.y = mouseY
 
         )
+    } else{
+        basic_tower.movalbilty_for_placement_towers === false
+        ctx.fillStyle = 'red';
+        ctx.fillCircle(mouseX,mouseY,basic_tower.attack_radius)
     }
 }
 
-if(MouseEvent('mouse_up') === true) {
-    basic_tower.movalbilty_for_placement_towers = false;
+
+    if(basic_tower.movalbilty_for_placement_towers === false + MouseEvent('click') === true === Sprite.position.x, Sprite.position.y){
+        ctx.fillStyle = ' red';
+        ctx.fillCircle(mouseX,mousey,50)
+    } 
+//better tower control and placment
+if(MouseEvent(mouseX) >= 71 && MouseEvent(mouseY)>= 506 && MouseEvent(click) === true && coins === 90 ){
+    coins - 90 
+    movalbilty_for_placement_towers === true
+    better_tower.position.x = mouseX
+    better_tower.position.y = mouseY
+    new Sprite(
+    better_tower.draw(
+        better_tower.position.x = mouseX,
+        better_tower.position.y = mouseY
+    )
+)
 }
+    
 
 
+function draw(){
+    this.draw
+}
 
 
 function animate() {
